533eb9acb57ee1abf16524b9fda5f94a26a73b45
zczhao1992 <zczhao1992@163.com>
2024-09-04 17:14:46 +0800

    fix: bug

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 import { protectServer } from "@/features/auth/utils";
 
 import { Banner } from "./banner";
-// import { ProjectsSection } from "./projects-section";
+import { ProjectsSection } from "./projects-section";
 import { TemplatesSection } from "./templates-section";
 
 export default async function Home() {
@@ -11,7 +11,7 @@ export default async function Home() {
     <div className="flex flex-col space-y-6 max-w-screen-xl mx-auto pb-10">
       <Banner />
       <TemplatesSection />
-      {/* <ProjectsSection /> */}
+      <ProjectsSection />
     </div>
   );
 }
